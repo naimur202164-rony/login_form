99715317cf509771853a6da4abda4aaf36cda377
naimur.a.rahman <naimur.a.rahman@doyenhub.com>
2022-04-11 18:50:45 +0600

    first commit

---
+++ src/Pages/Registar/Registar.js
@@ -0,0 +1,69 @@
+import { Button, Container, Grid, TextField, Typography } from '@mui/material';
+import React, { useState } from 'react';
+function Registar() {
+    const [loginData, setLoginData] = useState({});
+
+    
+
+    const handleOnBlur = e => {
+        e.preventDefault();
+        const field = e.target.name;
+        const value = e.target.value;
+        const newLoginData = { ...loginData };
+        newLoginData[field] = value;
+        setLoginData(newLoginData);
+        console.log(newLoginData);
+    }
+  
+  return (
+    <Container>
+            <Grid container spacing={2}>
+                <Grid item sx={{ mt: 8 }} xs={12} md={6}>
+                    <Typography variant="body1" gutterBottom>Register</Typography>
+                   <form onSubmit={handleOnBlur}>
+                        <TextField
+                            sx={{ width: '75%', m: 1 }}
+                            id="standard-basic"
+                            label="Your Name"
+                            name="name"
+                            onBlur={handleOnBlur}
+                            variant="standard" />
+                        <TextField
+                            sx={{ width: '75%', m: 1 }}
+                            id="standard-basic"
+                            label="Your Email"
+                            name="email"
+                            type="email"
+                            onBlur={handleOnBlur}
+                            variant="standard" />
+                        <TextField
+                            sx={{ width: '75%', m: 1 }}
+                            id="standard-basic"
+                            label="Your Password"
+                            type="password"
+                            name="password"
+                            onBlur={handleOnBlur}
+                            variant="standard" />
+                        <TextField
+                            sx={{ width: '75%', m: 1 }}
+                            id="standard-basic"
+                            label="ReType Your Password"
+                            type="password"
+                            name="password2"
+                            onBlur={handleOnBlur}
+                            variant="standard" />
+
+                        <Button sx={{ width: '75%', m: 1 }} type="submit" variant="contained">Register</Button>
+                       
+                    </form>
+                  
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    {/* <img style={{ width: '100%' }} src={login} alt="" /> */}
+                </Grid>
+            </Grid>
+        </Container>
+  )
+}
+
+export default Registar
---
+++ src/Pages/features/CreateDataSlice/CreateDataSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialState={
+    value:[{}]
+}
+
+export const createDataSlice=createSlice({
+    name:"data",
+    initialState,
+
+
+
+})
